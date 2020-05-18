18253b40e3959fa16e2a627f1acbc311fdcd304b
Terrence Wu <terrencewuxl@gmail.com>
2020-05-18 08:37:54 -0700

    generic search

--- leecode/src/main/java/com/leecode/pratice/generic/BinSearch.java
+++ leecode/src/main/java/com/leecode/pratice/generic/BinSearch.java
@@ -2,7 +2,7 @@ package com.leecode.pratice.generic;
 
 import java.util.Arrays;
 
-public class BinSearch{
+public class BinSearch {
 
     public static <T extends Comparable<T>> int search(T[] table, int start, int finish, T searchKey) {
         if (start > finish) return -1;
@@ -15,20 +15,20 @@ public class BinSearch{
         else return mid;
     }
 
-    public static <T extends Comparable<T>> int search(T[] table, T searchKey){
+    public static <T extends Comparable<T>> int search(T[] table, T searchKey) {
         Arrays.sort(table);
-        return search(table,0,table.length-1,searchKey);
+        return search(table, 0, table.length - 1, searchKey);
     }
 
-    public static void main(String[] args){
-        Integer[] arr1=new Integer[]{12,32,143,3,432,565,66,78,97};
-        System.out.println(BinSearch.search(arr1,78));
-        Character[] arr2=new Character[]{'a','f','r','g','l','d'};
-        System.out.println(BinSearch.search(arr2,'g'));
-        Tire compactTire=new Tire(15);
-        Tire suvTire=new Tire(17);
+    public static void main(String[] args) {
+        Integer[] arr1 = new Integer[]{12, 32, 143, 3, 432, 565, 66, 78, 97};
+        System.out.println(BinSearch.search(arr1, 78));
+        Character[] arr2 = new Character[]{'a', 'f', 'r', 'g', 'l', 'd'};
+        System.out.println(BinSearch.search(arr2, 'g'));
+        Tire compactTire = new Tire(15);
+        Tire suvTire = new Tire(17);
         Tire truckTire = new Tire(23);
-        Tire[] arr3=new Tire[]{compactTire,suvTire,truckTire};
-        System.out.println(BinSearch.search(arr3,truckTire));
+        Tire[] arr3 = new Tire[]{compactTire, suvTire, truckTire};
+        System.out.println(BinSearch.search(arr3, truckTire));
     }
 }
--- leecode/src/main/java/com/leecode/pratice/generic/Tire.java
+++ leecode/src/main/java/com/leecode/pratice/generic/Tire.java
@@ -1,16 +1,17 @@
 package com.leecode.pratice.generic;
 
-public class Tire implements Comparable<Tire>{
+public class Tire implements Comparable<Tire> {
 
     int size;
 
-    public Tire(int s){
-        size=s;
+    public Tire(int s) {
+        size = s;
     }
+
     @Override
     public int compareTo(Tire o) {
-        if(size<o.size)return -1;
-        else if(size>o.size)return 1;
+        if (size < o.size) return -1;
+        else if (size > o.size) return 1;
         return 0;
     }
 }
